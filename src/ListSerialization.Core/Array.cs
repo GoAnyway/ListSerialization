eaf343f081c812bcfb19b0dd86d3ae51c209b904
GoAnyway <woolftv@mail.ru>
2022-05-15 07:45:01 +0300

    Added zero-cost array.

---
+++ src/ListSerialization.Core/Array.cs
@@ -0,0 +1,55 @@
+﻿using System.Buffers;
+
+namespace ListSerialization.Core;
+
+public readonly ref struct Array<T>
+{
+    private readonly T[] _array;
+    private readonly bool _rented;
+
+    public Span<T> Span { get; }
+    public int Size { get; }
+
+    public Array(int size)
+    {
+        _array = ArrayPool<T>.Shared.Rent(size);
+        _rented = true;
+        Span = _array.AsSpan(..size);
+        Size = size;
+    }
+
+    public Array(Span<T> span)
+    {
+        _array = null;
+        _rented = false;
+        Span = span;
+        Size = span.Length;
+    }
+
+    public static implicit operator T[](Array<T> array) =>
+        array._array ?? throw new InvalidOperationException();
+    public static implicit operator Span<T>(Array<T> array) => array.Span;
+    public static implicit operator ReadOnlySpan<T>(Array<T> array) => array.Span;
+
+    public void Dispose()
+    {
+        if (!_rented) return;
+        ArrayPool<T>.Shared.Return(_array);
+    }
+
+    public T this[int i]
+    {
+        get => i < Size
+            ? Span[i]
+            : throw new ArgumentOutOfRangeException(nameof(i));
+        set => Span[i] = i < Size
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(i));
+    }
+
+    public Span<T> this[Range range] =>
+        range.Start.Value >= 0 &&
+        range.End.Value <= Size
+            ? Span[range]
+            : throw new ArgumentOutOfRangeException(nameof(range));
+}
