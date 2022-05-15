706680a9c5bbb8b7aa944a43360f44847ae595bf
GoAnyway <woolftv@mail.ru>
2022-05-15 08:02:19 +0300

    Made serialization and deserialization refactoring.

--- src/ListSerialization.Core/ListRandom.cs
+++ src/ListSerialization.Core/ListRandom.cs
@@ -1,90 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.Collections;
 
 namespace ListSerialization.Core
 {
     public class ListRandom
     {
+        public int Count;
         public ListNode Head;
         public ListNode Tail;
-        public int Count;
 
         public void Serialize(Stream stream)
         {
-            var elem = Head;
-            var list = new List<ListNode>();
-
-            var n = Count;
-            while (n-- > 0)
-            {
-                list.Add(elem);
-                elem = elem.Next;
-            }
-
-            WriteBytes(stream, BitConverter.GetBytes(Count));
-
-            foreach (var node in list)
-            {
-                WriteBytes(stream, BitConverter.GetBytes(list.IndexOf(node.Random)));
-                var bytes = Encoding.UTF8.GetBytes(node.Data);
-                WriteBytes(stream, BitConverter.GetBytes(bytes.Length));
-                WriteBytes(stream, bytes);
-            }
+            var serializer = new Serializer(this, stream);
+            serializer.Serialize();
         }
 
         public void Deserialize(Stream stream)
         {
-            Head = null;
-            Tail = null;
-
-            var buffer = new byte[sizeof(int)];
-            stream.Read(buffer);
-            Count = BitConverter.ToInt32(buffer);
-            var list = new List<ListNode>();
-            var randoms = new List<int>();
-
-            var n = Count;
-            while (n-- > 0)
-            {
-                buffer = new byte[sizeof(int)];
-                stream.Read(buffer);
-                var idx = BitConverter.ToInt32(buffer);
-                randoms.Add(idx);
-
-                stream.Read(buffer);
-                var size = BitConverter.ToInt32(buffer);
-
-                buffer = new byte[size];
-                stream.Read(buffer);
-                list.Add(new ListNode
-                {
-                    Data = Encoding.UTF8.GetString(buffer)
-                });
-            }
-
-            if (Count == 0) return;
-
-            Head = list[0];
-            Tail = list[Count - 1];
-
-            var elem = Head;
-            for (var idx = 0; idx < Count; ++idx)
-            {
-                elem.Random = list[randoms[idx]];
-                elem.Previous = idx > 0 ? list[idx - 1] : null;
-                elem.Next = idx < Count - 1 ? list[idx + 1] : null;
-                elem = elem.Next;
-            }
+            var serializer = new Serializer(this, stream);
+            serializer.Deserialize();
         }
 
-        private void WriteBytes(Stream stream, byte[] bytes)
+        private readonly struct Enumerable : IEnumerable<ListNode>
         {
-            foreach (var @byte in bytes)
+            private readonly ListRandom _list;
+
+            public Enumerable(ListRandom list)
             {
-                stream.WriteByte(@byte);
+                _list = list;
+            }
+
+            public IEnumerator<ListNode> GetEnumerator()
+            {
+                var current = _list.Head;
+                while (current is not null)
+                {
+                    yield return current;
+                    current = current.Next;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        private readonly struct Serializer
+        {
+            private readonly ListRandom _list;
+            private readonly Stream _stream;
+
+            public Serializer(ListRandom list, Stream stream)
+            {
+                _list = list;
+                _stream = stream;
+            }
+
+            public void Serialize()
+            {
+                _stream.Write(BitConverter.GetBytes(_list.Count));
+
+                var nodes = new Dictionary<ListNode, int>(_list.Count);
+                var idx = 0;
+                foreach (var node in new Enumerable(_list)) nodes.Add(node, idx++);
+
+                foreach (var (node, _) in nodes)
+                {
+                    _stream.Write(node.Data);
+                    var random = node.Random is not null
+                        ? nodes[node.Random]
+                        : -1;
+                    _stream.Write(random);
+                }
+            }
+
+            public void Deserialize()
+            {
+                _list.Count = _stream.ReadInt();
+                if (_list.Count == 0) return;
+
+                using var nodes = new Array<Node>(_list.Count);
+                for (var i = 0; i < _list.Count; i++)
+                {
+                    var node = DeserializeNode();
+                    nodes[i] = node;
+
+                    if (_list.Head is null)
+                    {
+                        _list.Head = node.ListNode;
+                        _list.Tail = _list.Head;
+                    }
+
+                    node.ListNode.Previous = _list.Tail;
+                    _list.Tail.Next = node.ListNode;
+                    _list.Tail = node.ListNode;
+                }
+
+                for (var i = 0; i < _list.Count; i++)
+                {
+                    var (node, random) = nodes[i];
+                    node.Random = nodes[random].ListNode;
+                }
+            }
+
+            private Node DeserializeNode()
+            {
+                var data = _stream.ReadString();
+                var random = _stream.ReadInt();
+
+                return new(new() { Data = data }, random);
             }
         }
+
+        private record Node(ListNode ListNode, int Random);
     }
 }
