a1b0c1753871a2393fb87b8bd82635888454fc25
GoAnyway <woolftv@mail.ru>
2022-05-15 10:27:12 +0300

    Optimized write and read methods.

--- src/ListSerialization.Core/StreamExtensions.cs
+++ src/ListSerialization.Core/StreamExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace ListSerialization.Core;
 
@@ -6,29 +7,39 @@ internal static class StreamExtensions
 {
     public static int ReadInt(this Stream stream)
     {
-        using var buffer = new Array<byte>(stackalloc byte[sizeof(int)]);
-        var read = stream.Read(buffer);
-        if (read != sizeof(int)) throw new InvalidOperationException();
+        var buffer = new Array<byte>(stackalloc byte[sizeof(int)]);
+        var _ = stream.Read(buffer);
 
         return BitConverter.ToInt32(buffer);
     }
 
     public static string ReadString(this Stream stream)
     {
+        const int stackallocThreshold = 1024;
         var size = stream.ReadInt();
-        using var buffer = new Array<byte>(size);
-        var read = stream.Read(buffer, 0, size);
-        if (read != size) throw new InvalidOperationException();
 
-        return Encoding.UTF8.GetString(buffer, 0, size);
+        using var buffer = size <= stackallocThreshold
+            ? new Array<byte>(stackalloc byte[size])
+            : new(size);
+        var _ = size <= stackallocThreshold
+            ? stream.Read(buffer)
+            : stream.Read(buffer, 0, size);
+
+        var chars = MemoryMarshal.Cast<byte, char>(buffer);
+        return new(chars);
     }
 
-    public static void Write(this Stream stream, int value) => 
-        stream.Write(BitConverter.GetBytes(value));
+    public static void Write(this Stream stream, int value)
+    {
+        var buffer = new Array<byte>(stackalloc byte[sizeof(int)]);
+        Unsafe.As<byte, int>(ref buffer.Span[0]) = value;
+        stream.Write(buffer);
+    }
 
     public static void Write(this Stream stream, string value)
     {
-        Write(stream, value.Length);
-        stream.Write(Encoding.UTF8.GetBytes(value));
+        var bytes = MemoryMarshal.Cast<char, byte>(value.AsSpan());
+        Write(stream, bytes.Length);
+        stream.Write(bytes);
     }
 }
